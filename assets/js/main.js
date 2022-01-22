09a87bb626ba6d9637056cef6f00df667b005934
RokuFSD <emilianordx@gmail.com>
2022-01-22 17:22:52 -0300

    preloader finished, starting settins

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,6 @@
-const gameBoard = (() => {
-  const gameBoard = new Array(9);
-  const setField = (position) => {
-    gameBoard[position] = player.getSign();
-  }
-  const getField = () => {
-  }
-  const checkEmpty = () => {
-  }
-
-  return {
-    setField,
-    getField,
-    checkEmpty
-  };
-})();
-
-const displayController = (() => {
-})();
-
-const displaySettings = (() => {
-})();
-
-const player = (symbol) => {
-  const getSign = () => {
-  }
-
-  const setSign = (character) => {
-  }
-
-  return {
-    getSign,
-    setSign
-  };
+function waitAnimation(){
+  const preloader = document.querySelector('.preload');
+  setInterval(() => {preloader.classList.add('preload--hide')},2000);
 }
+window.addEventListener('load', waitAnimation);
+
