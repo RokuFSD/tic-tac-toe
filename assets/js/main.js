9da0ec814ed98eef4225305a485794055088c8f1
RokuFSD <emilianordx@gmail.com>
2022-01-23 21:17:42 -0300

    finished animation preloader

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,32 @@
-function waitAnimation(){
-  const preloader = document.querySelector('.preload');
-  setInterval(() => {preloader.classList.add('preload--hide')},2000);
-}
-window.addEventListener('load', waitAnimation);
+const preloader = (() => {
+  const preloaderElement = document.querySelector('.preload');
+  const waitAnimation = () => {
+    setInterval(() => {
+      preloaderElement.classList.add('preload--hide');
+    }, 2000)
+  }
+  return {waitAnimation};
+})();
+
+const gameSettings = (() => {
+  const gameSettingsElement = document.querySelector('.gameSettings');
+  const button = document.querySelector('.btn');
+
+  const showUp = () => {
+    setTimeout(() => {
+      gameSettingsElement.classList.add('gameSettings--active');
+    }, 4200)
+  }
+
+  const hide = () => {
+    gameSettingsElement.classList.remove('gameSettings--active');
+  }
+
+  button.addEventListener("click", hide);
+  return {showUp};
+})();
+
+
+preloader.waitAnimation();
+gameSettings.showUp();
 
