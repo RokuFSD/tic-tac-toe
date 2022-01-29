f38b17e7e5e47c554948b6c0544da79644e960c8
RokuFSD <emilianordx@gmail.com>
2022-01-29 16:37:42 -0300

    TODO starting grid

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
 const preloader = (() => {
-  const preloaderElement = document.querySelector('.preload');
+  const preloaderElement = document.querySelector(".preload");
   const waitAnimation = () => {
     setInterval(() => {
-      preloaderElement.classList.add('preload--hide');
-    }, 2000)
-  }
-  return {waitAnimation};
+      preloaderElement.classList.add("preload--hide");
+    }, 2000);
+  };
+  return { waitAnimation };
 })();
 
 const Player = (name, type) => {
@@ -13,84 +13,131 @@ const Player = (name, type) => {
   let _type = type;
 
   const getName = () => {
-    return _name; 
-  }
+    return _name;
+  };
   const getType = () => {
     return _type;
-  }
+  };
 
-  const changeType = (type) => {
-    _type = type;
-  }
-
-  return {getName, getType, changeType};
-
-}
-
+  return { getName, getType};
+};
 
 const gameSettings = (() => {
-
   //Elements
-  const gameSettingsElement = document.querySelector('.gameSettings');
-  const button = document.querySelector('.btn');
-  const playersId = document.querySelectorAll('.playerCard__img');
-  const playerInputs = document.querySelectorAll('.player__name');
+  const gameSettingsElement = document.querySelector(".gameSettings");
+  const button = document.querySelector(".btn");
+  const playersId = document.querySelectorAll('[aria-data="person"]');
+  const playerInputs = document.querySelectorAll(".player__name");
+
+  let players = [];
+  let validated;
 
   //Functions
   const showUp = () => {
     setTimeout(() => {
-      gameSettingsElement.classList.add('gameSettings--active');
-    }, 4200)
-  }
+      gameSettingsElement.classList.add("gameSettings--active");
+    }, 4200);
+  };
 
   const hide = () => {
-    gameSettingsElement.classList.remove('gameSettings--active');
-  }
+    gameSettingsElement.classList.remove("gameSettings--active");
+  };
 
   const changeType = (player) => {
     const playerCurrentInput = player.target.nextElementSibling;
 
-    if(playerCurrentInput.classList.contains('player__name--cpu')){
-      player.target.src = 'assets/images/person.svg';
-      playerCurrentInput.classList.remove('player__name--cpu');
-      playerCurrentInput.placeholder = "Insert Name"
+    if (playerCurrentInput.classList.contains("player__name--cpu")) {
+      player.target.src = "assets/images/person.svg";
+      player.target.setAttribute("aria-data", "person");
+      playerCurrentInput.classList.remove("player__name--cpu");
+      playerCurrentInput.placeholder = "Insert Name";
       playerCurrentInput.value = "";
     } else {
-      player.target.src = 'assets/images/cpu.svg';
-      playerCurrentInput.classList.add('player__name--cpu');
+      player.target.setAttribute("aria-data", "cpu");
+      player.target.src = "assets/images/cpu.svg";
+      playerCurrentInput.classList.add("player__name--cpu");
       playerCurrentInput.placeholder = "CPU";
       playerCurrentInput.value = "CPU";
     }
-  }
+  };
+
+  const getValidated = () => {
+    return validated;
+  };
+
+  const setValidated = (booleanValue) => {
+    validated = booleanValue;
+  };
 
   const validateInput = () => {
-    playerInputs.forEach(input => {
-      if(input.value == "") return false;
-      return true;
-    })
-  }
+    if (playerInputs[0].value == "" || playerInputs[1].value == "") {
+      setValidated(false);
+    } else {
+      setValidated(true);
+    }
+
+    return getValidated();
+  };
+
+  const setPlayer = (name, type) => {
+    let newPlayer = Player(name, type);
+    players.push(newPlayer);
+  };
+
+  const getPlayer = () => {
+    return players;
+  };
 
   const submit = () => {
-    const validation = validateInput;
-    if(validation){
-      console.log("correcto");
+    if (validateInput()) {
+      setPlayer(playerInputs[0].value, playersId[0].getAttribute("aria-data"));
+      setPlayer(playerInputs[1].value, playersId[1].getAttribute("aria-data"));
+      hide();
     } else {
-      console.log("incorrecto");
+      alert("Name can't be empty");
+      return;
     }
-  }
+  };
 
   //Events
   button.addEventListener("click", submit);
-  playersId.forEach(player => {
+  playersId.forEach((player) => {
     player.addEventListener("click", changeType);
-  })
+  });
 
-  return {showUp};
+  return { showUp, button, getPlayer, getValidated };
 })();
 
+const gameController = (() => {
+  const mainBoard = document.querySelector(".gameContainer__grid");
+  const settings = gameSettings;
+  let playerOne = "";
+  let playerTwo = "";
+  
+  const start = () => {
+    [playerOne, playerTwo] = settings.getPlayer();
+  };
 
+  const showUp = () => {
+    mainBoard.parentElement.classList.add("gameContainer--active");
+  };
 
-preloader.waitAnimation();
-gameSettings.showUp();
+  return { showUp, start };
+})();
 
+const preload = preloader;
+const container = document.querySelector(".gameContainer");
+const settings = gameSettings;
+const controller = gameController;
+const submitButton = settings.button;
+
+preload.waitAnimation();
+settings.showUp();
+
+submitButton.addEventListener("click", () => {
+  if (settings.getValidated()) {
+    controller.showUp();
+    controller.start();
+  }
+});
 
